846dd56f15de16ccc301fe4a983059f3d5c0e659
icyboy <xupeng.js@gmail.com>
2020-11-20 10:06:29 +0800

    update secret.sh

--- deploy/secret.sh
+++ deploy/secret.sh
@@ -8,12 +8,12 @@ metadata:
     app: k8s-mutiboard-ldap
 type: Opaque
 data:
-  local_token: $(cat ./token/cluster1/token | base64)
-  local_ca.crt: $(cat ./token/cluster1/ca.crt | base64)
-  cnrelease_token: $(cat ./token/cluster2/token | base64)
-  cnrelease_ca.crt: $(cat ./token/cluster2/ca.crt | base64) 
-  usrelease_token: $(cat ./token/cluster3/token | base64)
-  usrelease_ca.crt: $(cat ./token/cluster3/ca.crt | base64)  
-  euprod_token: $(cat ./token/cluster4/token | base64)
-  euprod_ca.crt: $(cat ./token/cluster4/ca.crt | base64)     
+  local_token: $(openssl base64 -in ./token/cluster1/token -A)  
+  local_ca.crt: $(openssl base64 -in ./token/cluster1/ca.crt -A)
+  cnrelease_token: $(openssl base64 -in ./token/cluster2/token -A)  
+  cnrelease_ca.crt: $(openssl base64 -in ./token/cluster2/ca.crt -A)
+  usrelease_token: $(openssl base64 -in ./token/cluster3/token -A) 
+  usrelease_ca.crt: $(openssl base64 -in ./token/cluster3/ca.crt -A)
+  euprod_token: $(openssl base64 -in ./token/cluster4/token -A) 
+  euprod_ca.crt: $(openssl base64 -in ./token/cluster4/ca.crt -A)    
 EOF
